2d54cbb641fb8cf6b849b96cee1728a2867653f8
Cesar Herrera <cesarheal06@gmail.com>
2026-04-22 15:55:58 -0500

    feat: se realizo hasta el todo 12

--- mvc/practica_app/models/Libro.php
+++ mvc/practica_app/models/Libro.php
@@ -25,12 +25,64 @@ class Libro
     //   Sapiens (Harari), Clean Code (Martin), El principito (Saint-Exupéry)
     private static array $catalogo = [
         // TODO 11: agrega tus 5+ libros aquí
+        [
+        'id'          => 1,
+        'titulo'      => 'Cien años de soledad',
+        'autor'       => 'Gabriel García Márquez',
+        'genero'      => 'Novela',
+        'anio'        => 1967,
+        'precio'      => 45000,
+        'descripcion' => 'La magistral saga de la familia Buendía en el mítico pueblo de Macondo.',
+        'color'       => '#e67e22' // Naranja tierra
+    ],
+    [
+        'id'          => 2,
+        'titulo'      => 'Cosmos',
+        'autor'       => 'Carl Sagan',
+        'genero'      => 'Ciencia',
+        'anio'        => 1980,
+        'precio'      => 85000,
+        'descripcion' => 'Un viaje fascinante por la historia de la ciencia y el lugar de la humanidad en el universo.',
+        'color'       => '#2980b9' // Azul espacial
+    ],
+    [
+        'id'          => 3,
+        'titulo'      => 'Sapiens',
+        'autor'       => 'Yuval Noah Harari',
+        'genero'      => 'Historia',
+        'anio'        => 2011,
+        'precio'      => 69000,
+        'descripcion' => 'Un recorrido por la evolución humana, desde las cavernas hasta la era tecnológica.',
+        'color'       => '#7f8c8d' // Gris piedra
+    ],
+    [
+        'id'          => 4,
+        'titulo'      => 'Clean Code',
+        'autor'       => 'Robert C. Martin',
+        'genero'      => 'Tecnología',
+        'anio'        => 2008,
+        'precio'      => 120000,
+        'descripcion' => 'Manual de referencia para desarrollar código legible, mantenible y eficiente.',
+        'color'       => '#27ae60' // Verde código
+    ],
+    [
+        'id'          => 5,
+        'titulo'      => 'El principito',
+        'autor'       => 'Antoine de Saint-Exupéry',
+        'genero'      => 'Novela',
+        'anio'        => 1943,
+        'precio'      => 30000,
+        'descripcion' => 'Un relato poético sobre la naturaleza humana y la importancia de los vínculos.',
+        'color'       => '#f1c40f' // Amarillo estrella
+    ]
+
     ];
 
     // TODO 12: Implementa todos() — retorna todo el array $catalogo
     public static function todos(): array
     {
         // tu código aquí
+        return self::$catalogo;
     }
 
     // TODO 13: Implementa buscar($id) — retorna el libro con ese id,
@@ -39,6 +91,7 @@ class Libro
     public static function buscar(int $id): ?array
     {
         // tu código aquí
+        
     }
 
     // TODO 14: Implementa porGenero($genero) — retorna solo los libros
